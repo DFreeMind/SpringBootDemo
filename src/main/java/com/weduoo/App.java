11f3730c5fd61856d8a9557ffd43757d9495a305
露秋 <619069915@qq.com>
2019-05-21 23:24:43 +0800

    Springboot 基本结构

---
+++ src/main/java/com/weduoo/App.java
@@ -0,0 +1,23 @@
+package com.weduoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ApplicationContext ctx = SpringApplication.run(App.class, args);
+        String[] names = ctx.getBeanDefinitionNames();
+        Arrays.sort(names);
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
